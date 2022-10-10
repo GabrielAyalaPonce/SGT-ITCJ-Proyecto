0033aaed4ff863684fbe1bf32160ca241f10ec41
Angel Mendez <angelm.h.z@hotmail.com>
2022-10-10 13:21:40 -0600

    bugs login and form reactive HTML add

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,30 +12,35 @@ import { FirebaseCodeErrorsService } from 'src/app/services/firebase-code-errors
 })
 export class LoginComponent {
 
-  email!:string;
-  password!:string;
-  logemail!:string;
-  logpassword!:string;
+  //variables
   registerUser!: FormGroup;
   loginUser!:FormGroup;
   loading:boolean=false;
 
 
-  constructor(private afAuth:AngularFireAuth, private fb:FormBuilder,private snackBar:MatSnackBar,private router:Router,  private firebaseErrors:FirebaseCodeErrorsService ) {
+  constructor(private afAuth:AngularFireAuth,
+    private fb:FormBuilder,private snackBar:MatSnackBar,
+    private router:Router,
+    private firebaseErrors:FirebaseCodeErrorsService){
+       //form reactive register
       this.registerUser = this.fb.group({
-      email :['',Validators.required],
-      password :['',Validators.required],
+      email :['',[Validators.required, Validators.email]],
+      password :['',[Validators.required, Validators.minLength(6)]],
       repeatPassword :['',Validators.required],
       });
+    //form reactive login
       this.loginUser = this.fb.group({
-        logemail :['',Validators.required],
+        logemail :['',[Validators.required, Validators.email]],
         logpassword :['',Validators.required],
         })
    }
+
+   //registration method
    register() {
     const email = this.registerUser.value.email;
     const password = this.registerUser.value.password;
     const repeatPassword = this.registerUser.value.repeatPassword;
+    console.log(this.registerUser);
     if(password!=repeatPassword){
       this.snackBar.open('Contrasena deben coincidir','',{duration:1000})
     }
@@ -54,17 +59,17 @@ export class LoginComponent {
     console.log(email,password,repeatPassword);
   }
 
-  login() {
+
+  //registration method
+   login() {
     const email = this.loginUser.value.logemail;
     const password = this.loginUser.value.logpassword;
 
     this.loading = true;
     this.afAuth.signInWithEmailAndPassword(email, password).then((user) => {
-      if(user.user?.emailVerified) {
+      // if(user.user?.emailVerified) {
         this.router.navigate(['/dashboard']);
-      } else {
-        this.router.navigate(['/verificar-correo']);
-      }
+      // }
     }).catch((err) => {
       this.loading = false;
       this.snackBar.open(this.firebaseErrors.codeError(err.code), 'Aceptar');

89fca8e224b88dd844d44dc8612c593ada08fc65
Luis Angel <angelm.h.z@hotmail.com>
2022-12-07 16:58:58 -0700

    configuracion DB firebase

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { FirebaseCodeErrorsService } from 'src/app/services/firebase-code-errors.service';
+
 
 @Component({
   selector: 'app-login',
@@ -13,46 +14,55 @@ import { FirebaseCodeErrorsService } from 'src/app/services/firebase-code-errors
 export class LoginComponent {
 
   //variables
-  registerUser!: FormGroup;
-  loginUser!:FormGroup;
-  loading:boolean=false;
+  public registerUser!: FormGroup;
+  public loginUser!: FormGroup;
+  public loading: boolean = false;
 
 
-  constructor(private afAuth:AngularFireAuth,
-    private fb:FormBuilder,private snackBar:MatSnackBar,
-    private router:Router,
-    private firebaseErrors:FirebaseCodeErrorsService){
-       //form reactive register
-      this.registerUser = this.fb.group({
-      email :['',[Validators.required, Validators.email]],
-      password :['',[Validators.required, Validators.minLength(6)]],
-      repeatPassword :['',Validators.required],
-      });
+  constructor(private afAuth: AngularFireAuth,
+    private fb: FormBuilder, private snackBar: MatSnackBar,
+    private router: Router,
+    private firebaseErrors: FirebaseCodeErrorsService) {
+
+    //form reactive register
+    this.registerUser = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ['', Validators.required],
+      Ncontrol: ['', [Validators.required]],
+    });
+
     //form reactive login
-      this.loginUser = this.fb.group({
-        logemail :['',[Validators.required, Validators.email]],
-        logpassword :['',Validators.required],
-        })
-   }
+    this.loginUser = this.fb.group({
+      logemail: ['', [Validators.required, Validators.email]],
+      logpassword: ['', Validators.required],
+    })
+  }
 
-   //registration method
-   register() {
+  //registration method
+  register() {
     const email = this.registerUser.value.email;
     const password = this.registerUser.value.password;
     const repeatPassword = this.registerUser.value.repeatPassword;
+    const name = this.registerUser.value.name;
+    const uid = this.registerUser.value.uid;
+    const Ncontrol = this.registerUser.value.Ncontrol;
 
-    console.log(this.registerUser);
-    if(password!=repeatPassword){
-      this.snackBar.open('Contrasena deben coincidir','',{duration:1000});
+    if (password != repeatPassword) {
+      this.snackBar.open('Contrasena deben coincidir', '', { duration: 1000 });
       return;
     }
     this.loading = true;
-     this.afAuth.createUserWithEmailAndPassword(email,password).then(()=>{
+    
+   this.afAuth.createUserWithEmailAndPassword(email, password).then((userI) =>  {
+      console.log(userI.user?.uid) 
       this.verificarCorreo();
-    }).catch((err)=>{
+    }).catch((err) => {
       this.loading = false;
       this.snackBar.open(this.firebaseErrors.codeError(err.code), 'Aceptar');
     })
+     console.log()
   }
 
 
@@ -60,21 +70,23 @@ export class LoginComponent {
     this.afAuth.currentUser.then((user) => user?.sendEmailVerification())
       .then(() => {
         this.router.navigate(['/verify-email']);
-        this.snackBar.open( 'Le enviamos un correo electronico para su verificacion','Aceptar'
+        this.snackBar.open('Le enviamos un correo electronico para su verificacion', 'Aceptar'
         );
       });
-    }
+  }
 
   //login method
-   login() {
+  login() {
     const email = this.loginUser.value.logemail;
     const password = this.loginUser.value.logpassword;
 
     this.loading = true;
     this.afAuth.signInWithEmailAndPassword(email, password).then((user) => {
-      if(user.user?.emailVerified) {
+      if (user.user?.emailVerified) {
         this.router.navigate(['/dashboard']);
-      }else{
+        console.log('id de usuario',user.user?.uid)
+        console.log('id de usuario',user.user?.email)
+      } else {
         this.router.navigate(['/verify-email'])
       }
     }).catch((err) => {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sgt-firebase-adb81',
+    appId: '1:684061822800:web:6859f426a99edbb3732655',
+    storageBucket: 'sgt-firebase-adb81.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA1L6c0racfI7Xiyiw9jxZ4KBlWDqYk5Bc',
+    authDomain: 'sgt-firebase-adb81.firebaseapp.com',
+    messagingSenderId: '684061822800',
+    measurementId: 'G-RCQVPXZ2KT',
+  },
   production: false,
   firebaseConfig:{
     apiKey: "AIzaSyA1L6c0racfI7Xiyiw9jxZ4KBlWDqYk5Bc",

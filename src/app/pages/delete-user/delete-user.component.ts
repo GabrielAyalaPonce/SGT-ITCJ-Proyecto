6ea3ebf544d69b1d1dcb0dfd9a12ebbc6f36b4a2
Luis Angel Mendez Hernandez <angelm.h.z@hotmail.com>
2023-02-07 11:32:16 -0700

    Datos socioeconomicos terminados

--- src/app/pages/delete-user/delete-user.component.ts
+++ src/app/pages/delete-user/delete-user.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteUserComponent implements OnInit {
 
+
+public mostrarResultados: boolean = false;
+public nombre: any;
+public correo: any;
+public numeroControl: any;
+public carrera: any;
+public rol: any;
+
+buscarUsuario() {
+ //lógica para buscar el usuario,
+  this.nombre = "Luis Angel Mendez Hernandez";
+  this.correo = "l18110529@itcj.edu.mx";
+  this.numeroControl = "181110529";
+  this.carrera = "Ingeniería en Sistemas";
+  this.rol = "Tutorado";
+  this.mostrarResultados = true;
+}
+
   constructor() { }
 
   ngOnInit(): void {

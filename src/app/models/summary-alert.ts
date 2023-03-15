129fb380e7832693763f20442cc02ba7f8d345c1
Gabriel <L17111956@itcj.edu.mx>
2023-03-15 16:52:25 -0600

    Resumen de alertas en proceso falta obtener datos

---
+++ src/app/models/summary-alert.ts
@@ -0,0 +1,13 @@
+export interface UserTableData {
+    id:any;
+    name: any,
+    Ncontrol: any,
+    EN$: any,
+    N$: any,
+    ADA: any,
+    BDA: any,
+    SM: any,
+    AE: any,
+    Psicologia: any
+}
+    
--- src/app/pages/alert-summary/alert-summary.component.ts
+++ src/app/pages/alert-summary/alert-summary.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserTableData } from 'src/app/models/summary-alert';
+import { UserFirebaseService } from 'src/app/services/user-firebase.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { User } from 'src/app/models/user';
+
 
 @Component({
   selector: 'app-alert-summary',
@@ -6,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert-summary.component.css']
 })
 export class AlertSummaryComponent implements OnInit {
+  users: User[] = [
+  ];
+  dataSource = new MatTableDataSource<User>([]);
+  columnsToDisplay : string[] = ['id','name','Ncontrol'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { }
+  //,'EN$','N$','ADA','BDA','SM','AE','Psicologia'
+  constructor(private userfirebaseservice: UserFirebaseService) { }
 
   ngOnInit(): void {
+    this.userfirebaseservice.getUsers().subscribe(users => {
+      console.log('Mostrando usuarios',users)
+      this.dataSource = new MatTableDataSource(this.users);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      
+    });
   }
 
 }

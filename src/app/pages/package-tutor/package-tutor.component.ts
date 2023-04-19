fee9a36b12f515a354774ab2e7e6de48b499576a
sistemas <sistemas@injectronic.com>
2023-04-19 14:03:34 -0600

    boton publicar paquete

--- src/app/pages/package-tutor/package-tutor.component.ts
+++ src/app/pages/package-tutor/package-tutor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { PackagesService } from 'src/app/services/packages.service';
 import * as Notiflix from 'notiflix';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-package-tutor',
@@ -9,34 +10,34 @@ import * as Notiflix from 'notiflix';
   styleUrls: ['./package-tutor.component.css']
 })
 export class PackageTutorComponent implements OnInit {
-  paquetesAsignados: any[] = []; 
+  paquetesAsignados: any[] = [];
   panelOpenState = false;
-  last:any;
-  subjectsAndSchedules : any[] = [];
+  last: any;
+  subjectsAndSchedules: any[] = [];
   editandoClave = false;
+  publicados: { [packageId: string]: boolean } = {};
 
-  saveKeyAuthorizationedit(paquete:any) {
+  saveKeyAuthorizationedit(paquete: any) {
     this.editandoClave = true;
   }
 
-  constructor(private firestore: AngularFirestore, private packagesService: PackagesService ) { }
+  constructor(private firestore: AngularFirestore, private packagesService: PackagesService) { }
 
   ngOnInit(): void {
 
-    
+
     Notiflix.Notify.init({
       width: '50%',
-      position: 'center-center', 
+      position: 'center-center',
       fontSize: '1em',
       borderRadius: '5px',
     });
- 
+
     this.packagesService.getPackages().subscribe(
-      resp=> console.log('paquetes', resp)
+      resp => console.log('paquetes', resp)
     )
     this.obtenerPaqueteAsignado('VVXGIPI4ttMZzTQRzLKuPxi0sDs1'); // Reemplaza esto con el ID del usuario actual.
   }
-
   obtenerPaqueteAsignado(uid: string): void {
     console.log('UID:', uid);
     this.firestore.collection('packages', ref => ref.where('TutorAsignado.uid', '==', uid)).valueChanges().subscribe(data => {
@@ -44,6 +45,10 @@ export class PackageTutorComponent implements OnInit {
       if (data.length > 0) {
         this.paquetesAsignados = data;
         console.log(this.paquetesAsignados);
+        // Asignar el estado de publicación
+        this.paquetesAsignados.forEach(paquete => {
+          this.publicados[paquete.id] = paquete.post;
+        });
       } else {
         console.log('No se encontró paquete asignado.');
       }
@@ -51,6 +56,50 @@ export class PackageTutorComponent implements OnInit {
   }
 
 
+  togglePublicarPaquete(packageId: string): void {
+    const paquete = this.paquetesAsignados.find(p => p.id === packageId);
+    const postActual = paquete.post;
+
+    if (!paquete.keyAuthorization) {
+      Notiflix.Confirm.show(
+        'Confirmación',
+        'El paquete no tiene clave de autorización, lo que significa que cualquier persona puede ingresar. ¿continuar?',
+        'Sí',
+        'No',
+        () => {
+          this.publicarPaqueteConfirmado(packageId, !postActual);
+        },
+        () => {
+        }
+      );
+    } else {
+      this.publicarPaqueteConfirmado(packageId, !postActual);
+    }
+  }
+
+  publicarPaqueteConfirmado(packageId: string, post: boolean): void {
+    const updatedData: any = {
+      post: post
+    };
+    this.packagesService.updatePackage(packageId, updatedData).subscribe(
+      () => {
+        console.log('Paquete actualizado correctamente');
+        this.publicados[packageId] = post;
+        if (post) {
+          Notiflix.Notify.success(`El paquete se publicó exitosamente`, {
+            timeout: 2000
+          });
+        } else {
+          Notiflix.Notify.warning('El paquete ya no está publicado', {
+            timeout: 2000
+          });
+        }
+      },
+      (error) => {
+        console.error('Error actualizando el paquete:', error);
+      }
+    );
+  }
 
   saveKeyAuthorization(paquete: any): void {
     const updatedData: any = {
@@ -60,11 +109,10 @@ export class PackageTutorComponent implements OnInit {
     this.packagesService.updatePackage(paquete.id, updatedData).subscribe(() => {
       console.log('KeyAuthorization actualizada con éxito');
       this.editandoClave = false;
-     Notiflix.Notify.success('Clave guardada con exito');
+      Notiflix.Notify.success('Clave guardada con exito');
     }, error => {
       console.error('Error al actualizar KeyAuthorization', error);
     });
   }
-  
-}
 
+}
